260f6ba0bfe907932657900da82e8840b13b2548
hunterdw0007 <hunterw.mail@gmail.com>
2023-01-31 19:09:44 -0600

    initial commit

---
+++ lua/hunter/core/options.lua
@@ -0,0 +1,39 @@
+local opt = vim.opt
+
+-- line numbers
+opt.relativenumber = true
+opt.number = true
+
+-- tabs
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.autoindent = true
+
+-- line wrapping
+opt.wrap = false
+
+-- search settings
+opt.ignorecase = true
+opt.smartcase = true
+
+-- cursor line
+opt.cursorline = false
+
+-- appearance
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+-- backspace
+opt.backspace = "indent,eol,start"
+
+-- clipboard
+opt.clipboard:append("unnamedplus")
+
+-- split windows
+opt.splitbelow = true
+opt.splitright = true
+
+opt.iskeyword:append("-")
+
